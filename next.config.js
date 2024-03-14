9c1be4a45e9fff705dc6e9c9f16b795d1db999ac
Hakxel <asiel_montes@yahoo.com>
2024-03-14 13:30:05 -0400

    Changes for chapters 7-14

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    // Enable source maps in development mode
+    if (dev) {
+      config.devtool = "source-map";
+    }
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
